f6252426043a37246e7d751c7955413a74c7820b
Bruno Mello <sbrunomello@gmail.com>
2020-01-19 18:15:32 -0300

    heroku

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-//Carrega o módulo server.js
-var Server = require('./server'),
-    
-// //Carrega o objeto io
-// socketServer = Server.socketServer,
-  
-// //Carrega o objeto httpServer
-// webServer = Server.webServer;
+var express = require('express')
+var app = express()
 
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/views'))
+
+// app.get('/', function(request, response) {
+//   response.send('Hello World!')
+// })
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
