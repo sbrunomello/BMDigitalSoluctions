55071839e9ba8dc08d2e71120b9f80cfb83e6d50
Bruno Mello <sbrunomello@gmail.com>
2020-01-20 20:21:49 -0300

    up

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ app.use(express.static(__dirname + '/views'))
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+app.get('*', (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] != 'https') {
+      
+// checa se o header é HTTP ou HTTPS
+
+      res.redirect("https://" + req.headers.host + req.url);
+      
+// faz o redirect para HTTPS
+
+  } else {
+      next();
+      
+// segue com a sequência das rotas
+
+  }
+});
